3b51595a5afb40343fbaaa715016fb60ffb6eb5b
Aishwarya Hariharan Iyer <95858016+Aishwarya-Hariharan-Iyer@users.noreply.github.com>
2026-06-10 03:37:43 +0530

    Implement intersection of two arrays function

---
+++ python_problems/easy/349-intersection-of-two-arrays.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1 = list(set(nums1))
+        nums2 = list(set(nums2))
+        intersection = []
+        for n in nums1:
+            if n in nums2:
+                intersection.append(n)
+
+        return intersection
