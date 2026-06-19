53f2cb040d9bdd1eb20d1e34108dc210ee04f5eb
Aishwarya Hariharan Iyer <95858016+Aishwarya-Hariharan-Iyer@users.noreply.github.com>
2026-06-19 14:14:01 +0530

    Implement minimumDifference method for score differences

---
+++ python_problems/easy/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+
+        if k == 1:
+            return 0
+
+        nums.sort()
+        minimum = float('inf')
+
+        for i in range(len(nums)-k+1):
+            minimum = min(minimum, nums[i+k-1] - nums[i])
+        
+        return minimum
